b06280349c93e6a746867109155551a1afbb272b
Shubham Sachdeva <shubham.sachdeva0499@gmail.com>
2021-04-22 20:10:56 -0400

    added dissolve effect

--- Assets/Scripts/Powerups/MultipleBulletsPowerup.cs
+++ Assets/Scripts/Powerups/MultipleBulletsPowerup.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 class MultipleBulletsPowerup : Powerup {
-
+    public Material currentMaterial;
+    public Material rimLighting;
 
     public override void applyEffect(PlayerController controller) {
         controller.multiBullet = true;
+        controller.gameObject.GetComponent<MeshRenderer>().material = rimLighting;
     }
 
     public override void removeEffect(PlayerController controller) {
         controller.multiBullet = false;
+        controller.gameObject.GetComponent<MeshRenderer>().material = currentMaterial;
     }
 }
