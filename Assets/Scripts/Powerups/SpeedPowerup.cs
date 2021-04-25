8096e9b0996cbee5af7257d081afec33b1d87946
Shubham Sachdeva <shubham.sachdeva0499@gmail.com>
2021-04-25 18:48:35 -0400

    rim lighting fixed

--- Assets/Scripts/Powerups/SpeedPowerup.cs
+++ Assets/Scripts/Powerups/SpeedPowerup.cs
@@ -6,15 +6,21 @@ class SpeedPowerup : Powerup {
 
     public float powerupSpeed;
     float playerOriginalSped;
+    public Material currentMaterial;
+    public Material rimLighting;
 
     public override void applyEffect(PlayerController controller) {
         playerOriginalSped = controller.movementSpeed;
         controller.movementSpeed = powerupSpeed;
         if (hudManager)
             hudManager.StartPowerupTimer(this, effectTime);
+        MeshRenderer bodyRenderer = controller.gameObject.transform.Find("Mesh").GetComponent<MeshRenderer>();
+        bodyRenderer.material = rimLighting;
     }
 
     public override void removeEffect(PlayerController controller) {
         controller.movementSpeed = playerOriginalSped;
+        MeshRenderer bodyRenderer = controller.gameObject.transform.Find("Mesh").GetComponent<MeshRenderer>();
+        bodyRenderer.material = currentMaterial;
     }
 }
