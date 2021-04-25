371c9e18ac0d5eb28a8a8bdc68de5c3cc58e29f4
Shubham Sachdeva <shubham.sachdeva0499@gmail.com>
2021-04-25 18:04:07 -0400

    rim lighting fixed

--- Assets/Scripts/Powerups/MultipleBulletsPowerup.cs
+++ Assets/Scripts/Powerups/MultipleBulletsPowerup.cs
@@ -10,11 +10,13 @@ class MultipleBulletsPowerup : Powerup {
         controller.multiBullet = true;
         if (hudManager)
             hudManager.StartPowerupTimer(this, effectTime);
-        controller.gameObject.GetComponent<MeshRenderer>().material = rimLighting;
+        MeshRenderer bodyRenderer = controller.gameObject.transform.Find("Mesh").GetComponent<MeshRenderer>();
+        bodyRenderer.material = rimLighting;
     }
 
     public override void removeEffect(PlayerController controller) {
         controller.multiBullet = false;
-        controller.gameObject.GetComponent<MeshRenderer>().material = currentMaterial;
+        MeshRenderer bodyRenderer = controller.gameObject.transform.Find("Mesh").GetComponent<MeshRenderer>();
+        bodyRenderer.material = currentMaterial;
     }
 }
